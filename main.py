db4fd5245409a5053006dd7ae833110982ea3210
doubleshendi <78292834+doubleshendi@users.noreply.github.com>
2024-05-26 00:56:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#def rolldice():
+  #import random
+  #dice = random.randint(1, 6)
+  #print("You roolled", dice)
+
+#for i in range(10):
+#rolldice()
+
+print("REPLIT LOGIN SYSTEM")
+print()
+def login():
+  while True:
+    username = input("What is your username?: ")
+    password = input("What is your password?: ")
+    if username == "suchita" and password == "YouSLAYGirlyPop":
+      print("Welcome to Replit!")
+      break
+    else:
+      print("Whoops! I don't recognize that username or password. Try again!")
+      continue
+
+login()
+  
